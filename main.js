5e64fa42810f5049c2fb034d5ded70d4b58ad0cd
24balaji <99334076+24balaji@users.noreply.github.com>
2022-09-04 15:05:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(300 , 300);
+canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('my Filtered Image.png');
+}
+
+function Lipstick() {
+    window.location = "index2.html"
+}
+
+function mustach() {
+    window.location = "index.html"
+}
+
+
+
+
+
+
+
+
+
