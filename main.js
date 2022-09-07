852a2d42d22e175992ace1b95d32043a17873a53
24balaji <99334076+24balaji@users.noreply.github.com>
2022-09-07 18:06:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,15 +2,35 @@ function preload(){
 }
 
 function setup(){
-canvas = createCanvas(300 , 300);
+canvas = createCanvas(350 , 300);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(350,300);
+video.hide();
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
 }
 
 function draw() {
+    image(video,0,0,350,300);
 }
 
 function take_snapshot() {
     save('my Filtered Image.png');
+}
+
+function modelLoaded() {
+    console.log('Posenet Is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+       console.log(results);
+      console.log("nose x = " + results[0].pose.nose.x-15);
+      console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
 
 function Lipstick() {
